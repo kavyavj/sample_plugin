c8d71876875be55faa4d63a4bbe85921d0710762
kavya V J <kavya.vj@connovatech.com>
2014-04-28 19:34:07 +0530

    Testing if contents can be put to seed

---
+++ lib/generators/sample_plugin/seed_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'active_record'
+require 'rails/generators/active_record'
+
+module SamplePlugin
+  module Generators
+    class SeedGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      vendor "seeds.rb" do
+        p "seed file test"
+      end
+
+      
+    end
+  end
+end
