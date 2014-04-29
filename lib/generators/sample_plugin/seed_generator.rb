4114e4e6833e01be7eefefee6e5d3d52b93ea097
kavya V J <kavya.vj@connovatech.com>
2014-04-29 10:53:08 +0530

    seed genrator

--- lib/generators/sample_plugin/seed_generator.rb
+++ lib/generators/sample_plugin/seed_generator.rb
@@ -1,10 +1,19 @@
-class SeedGenerator < Rails::Generators::Base
-  def create_initializer_file
-  	vendor "seeds.rb" do
-	  hickwall = Hickwall.new
-		hickwall.hello("Hello World")
-		hickwall.save!
-	end
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'active_record'
+require 'rails/generators/active_record'
 
+module SamplePlugin
+  module Generators
+    class SeedGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      def create_initializer_file
+	  	vendor "seeds.rb" do
+		  hickwall = Hickwall.new
+			hickwall.hello("Hello World")
+			hickwall.save!
+		end
+    end
   end
 end
