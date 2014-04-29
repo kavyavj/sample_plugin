03bc5057efcaaaec815030a9884567aa72191d63
kavya V J <kavya.vj@connovatech.com>
2014-04-29 11:18:45 +0530

    Removed complicated generator method,add content to table generated

--- lib/generators/sample_plugin/install_generator.rb
+++ lib/generators/sample_plugin/install_generator.rb
@@ -6,31 +6,37 @@ require 'rails/generators/active_record'
 module SamplePlugin
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+      # include Rails::Generators::Migration
 
-      source_root File.expand_path("../templates", __FILE__)
+      # source_root File.expand_path("../templates", __FILE__)
 
-      # Implement the required interface for Rails::Generators::Migration.
-      def self.next_migration_number(dirname) #:nodoc:
-        next_migration_number = current_migration_number(dirname) + 1
-        if ActiveRecord::Base.timestamped_migrations
-          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-        else
-          "%.3d" % next_migration_number
-        end
-      end
+      # # Implement the required interface for Rails::Generators::Migration.
+      # def self.next_migration_number(dirname) #:nodoc:
+      #   next_migration_number = current_migration_number(dirname) + 1
+      #   if ActiveRecord::Base.timestamped_migrations
+      #     [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+      #   else
+      #     "%.3d" % next_migration_number
+      #   end
+      # end
 
-      def copy_migration
-        migration_template 'install.rb', 'db/migrate/install_sample_plugin.rb'
-      end
+      # def copy_migration
+      #   migration_template 'install.rb', 'db/migrate/install_sample_plugin.rb'
+      # end
+
+      
+
+      def generate_forums_table
+        generate "scaffold", "forums title:string description:text"
+      end 
 
       def create_seed_file
           vendor "seeds.rb" do
-            # Table created by plugin
-            # test = Test.new
-            # test.test_id = 234
-            # test.test_name = "plgin test"
-            # test.save!
+            #Table created by plugin
+            forum = Forum.new
+            forum.title = "ABCD"
+            forum.description = "kalskdn hfhfhf fhfheu juurriowkwj"
+            forum.save!
 
             #Table that is in application
             hickwall = Hickwall.new
@@ -38,10 +44,7 @@ module SamplePlugin
             hickwall.save!
         end
       end
-
-      def generate_forums_table
-        generate "scaffold", "forums title:string description:text"
-      end  
+      
 
 
     end
